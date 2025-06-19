f96e25a89a41755bee1dcd0acdac92289acb52be
Luis Correa <luisfacul1.2@gmail.com>
2025-06-19 14:43:03 -0300

    Voice upgrade

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+
 function exibirTextoNaTela(tag, texto){
         let campo = document.querySelector(tag);
-        campo.innerHTML = texto;        
+        campo.innerHTML = texto;
+        if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }       
 }
 function exibirMensagemIncial(){
 exibirTextoNaTela('h1','Jogo do número secreto');
@@ -30,7 +41,20 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNalista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNalista == numeroLimite) { 
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){ 
+        return gerarNumeroAleatorio();
+    }else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+
 }
 function limparCampo() { 
     chute = document.querySelector('input');
