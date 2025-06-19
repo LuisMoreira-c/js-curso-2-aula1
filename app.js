571e5bd21f43d60da74a2688c189b83353acf3f6
Luis Correa <luisfacul1.2@gmail.com>
2025-06-19 14:02:36 -0300

    Butao novo jogo atualizado

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ function exibirTextoNaTela(tag, texto){
         let campo = document.querySelector(tag);
         campo.innerHTML = texto;        
 }
-
+function exibirMensagemIncial(){
 exibirTextoNaTela('h1','Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
+}
+exibirMensagemIncial();
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
@@ -34,4 +35,11 @@ function gerarNumeroAleatorio() {
 function limparCampo() { 
     chute = document.querySelector('input');
     chute.value = '';
+}
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemIncial();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
 }
